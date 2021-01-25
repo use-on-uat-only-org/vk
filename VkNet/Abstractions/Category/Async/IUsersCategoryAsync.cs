a212bef78bd5d0d57ccb3447545b38d46c91d2c0
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2021-01-25 23:02:18 +0300

    #1095 Добавляет CancellationToken в IUsersCategoryAsync

--- VkNet/Abstractions/Category/Async/IUsersCategoryAsync.cs
+++ VkNet/Abstractions/Category/Async/IUsersCategoryAsync.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using VkNet.Enums.Filters;
 using VkNet.Enums.SafetyEnums;
 using VkNet.Model;
 using VkNet.Model.Group;
-using VkNet.Model.RequestParams;
 using VkNet.Model.RequestParams.Users;
 using VkNet.Utils;
 
@@ -22,6 +22,7 @@ namespace VkNet.Abstractions
 		/// Возвращает список пользователей в соответствии с заданным критерием поиска.
 		/// </summary>
 		/// <param name="params"> Параметры запроса. </param>
+		/// <param name="token"></param>
 		/// <returns>
 		/// После успешного выполнения возвращает список объектов пользователей, найденных
 		/// в соответствии с заданными
@@ -31,7 +32,7 @@ namespace VkNet.Abstractions
 		/// <remarks>
 		/// Страница документации ВКонтакте http://vk.com/dev/users.search
 		/// </remarks>
-		Task<VkCollection<User>> SearchAsync(UserSearchParams @params);
+		Task<VkCollection<User>> SearchAsync(UserSearchParams @params, CancellationToken token = default);
 
 		/// <summary>
 		/// Возвращает информацию о том, установил ли пользователь приложение.
@@ -41,6 +42,7 @@ namespace VkNet.Abstractions
 		/// пользователя (Целое
 		/// число, по умолчанию идентификатор текущего пользователя).
 		/// </param>
+		/// <param name="token"></param>
 		/// <returns>
 		/// После успешного выполнения возвращает 1 в случае, если пользователь установил у
 		/// себя данное приложение, иначе 0.
@@ -48,7 +50,7 @@ namespace VkNet.Abstractions
 		/// <remarks>
 		/// Страница документации ВКонтакте http://vk.com/dev/users.isAppUser
 		/// </remarks>
-		Task<bool> IsAppUserAsync(long? userId);
+		Task<bool> IsAppUserAsync(long? userId, CancellationToken token = default);
 
 		/// <summary>
 		/// Возвращает расширенную информацию о пользователях. Метод не требует авторизацию
@@ -59,14 +61,13 @@ namespace VkNet.Abstractions
 		/// </param>
 		/// <param name="fields"> Поля профилей, которые необходимо возвратить. </param>
 		/// <param name="nameCase"> Падеж для склонения имени и фамилии пользователя </param>
+		/// <param name="token"></param>
 		/// <returns> Список объектов с запрошенной информацией о пользователях. </returns>
 		/// <remarks>
 		/// Страница документации ВКонтакте http://vk.com/dev/users.get
 		/// </remarks>
-		Task<ReadOnlyCollection<User>> GetAsync([NotNull]
-												IEnumerable<long> userIds
-												, ProfileFields fields = null
-												, NameCase nameCase = null);
+		Task<ReadOnlyCollection<User>> GetAsync([NotNull] IEnumerable<long> userIds, ProfileFields fields = null, NameCase nameCase = null,
+												CancellationToken token = default);
 
 		/// <summary>
 		/// Возвращает расширенную информацию о пользователях.
@@ -77,14 +78,13 @@ namespace VkNet.Abstractions
 		/// </param>
 		/// <param name="fields"> Поля профилей, которые необходимо возвратить. </param>
 		/// <param name="nameCase"> Падеж для склонения имени и фамилии пользователя </param>
+		/// <param name="token"></param>
 		/// <returns> Список объектов с запрошенной информацией о пользователях. </returns>
 		/// <remarks>
 		/// Страница документации ВКонтакте http://vk.com/dev/users.get
 		/// </remarks>
-		Task<ReadOnlyCollection<User>> GetAsync([NotNull]
-												IEnumerable<string> screenNames
-												, ProfileFields fields = null
-												, NameCase nameCase = null);
+		Task<ReadOnlyCollection<User>> GetAsync([NotNull] IEnumerable<string> screenNames, ProfileFields fields = null,
+												NameCase nameCase = null, CancellationToken token = default);
 
 		/// <summary>
 		/// Возвращает список идентификаторов пользователей и групп, которые входят в
@@ -103,16 +103,15 @@ namespace VkNet.Abstractions
 		/// Список дополнительных полей для объектов user и group, которые необходимо
 		/// вернуть.
 		/// </param>
+		/// <param name="token"></param>
 		/// <returns>
 		/// Пока возвращается только список групп.
 		/// </returns>
 		/// <remarks>
 		/// Страница документации ВКонтакте http://vk.com/dev/users.getSubscriptions
 		/// </remarks>
-		Task<VkCollection<Group>> GetSubscriptionsAsync(long? userId = null
-														, int? count = null
-														, int? offset = null
-														, GroupsFields fields = null);
+		Task<VkCollection<Group>> GetSubscriptionsAsync(long? userId = null, int? count = null, int? offset = null,
+														GroupsFields fields = null, CancellationToken token = default);
 
 		/// <summary>
 		/// Возвращает список идентификаторов пользователей, которые являются подписчиками
@@ -129,15 +128,13 @@ namespace VkNet.Abstractions
 		/// </param>
 		/// <param name="fields"> Список дополнительных полей, которые необходимо вернуть </param>
 		/// <param name="nameCase"> Падеж для склонения имени и фамилии пользователя </param>
+		/// <param name="token"></param>
 		/// <returns> Список подписчиков </returns>
 		/// <remarks>
 		/// Страница документации ВКонтакте http://vk.com/dev/users.getFollowers
 		/// </remarks>
-		Task<VkCollection<User>> GetFollowersAsync(long? userId = null
-													, int? count = null
-													, int? offset = null
-													, ProfileFields fields = null
-													, NameCase nameCase = null);
+		Task<VkCollection<User>> GetFollowersAsync(long? userId = null, int? count = null, int? offset = null, ProfileFields fields = null,
+													NameCase nameCase = null, CancellationToken token = default);
 
 		/// <summary>
 		/// Позволяет пожаловаться на пользователя.
@@ -148,23 +145,25 @@ namespace VkNet.Abstractions
 		/// </param>
 		/// <param name="type"> Тип жалобы </param>
 		/// <param name="comment"> Комментарий к жалобе на пользователя </param>
+		/// <param name="token"></param>
 		/// <returns> В случае успешной жалобы метод вернет true. </returns>
 		/// <remarks>
 		/// Страница документации ВКонтакте http://vk.com/dev/users.report
 		/// </remarks>
-		Task<bool> ReportAsync(long userId, ReportType type, string comment = "");
+		Task<bool> ReportAsync(long userId, ReportType type, string comment = "", CancellationToken token = default);
 
 		/// <summary>
 		/// Индексирует текущее местоположение пользователя и возвращает список
 		/// пользователей, которые находятся вблизи.
 		/// </summary>
 		/// <param name="params"> Входные параметры выборки. </param>
+		/// <param name="token"></param>
 		/// <returns>
 		/// После успешного выполнения возвращает список объектов user.
 		/// </returns>
 		/// <remarks>
 		/// Страница документации ВКонтакте http://vk.com/dev/users.getNearby
 		/// </remarks>
-		Task<VkCollection<User>> GetNearbyAsync(UsersGetNearbyParams @params);
+		Task<VkCollection<User>> GetNearbyAsync(UsersGetNearbyParams @params, CancellationToken token = default);
 	}
 }
--- VkNet/Categories/Async/UsersCategoryAsync.cs
+++ VkNet/Categories/Async/UsersCategoryAsync.cs
@@ -1,11 +1,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading;
 using System.Threading.Tasks;
 using VkNet.Enums.Filters;
 using VkNet.Enums.SafetyEnums;
 using VkNet.Model;
 using VkNet.Model.Group;
-using VkNet.Model.RequestParams;
 using VkNet.Model.RequestParams.Users;
 using VkNet.Utils;
 
@@ -15,73 +15,56 @@ namespace VkNet.Categories
 	public partial class UsersCategory
 	{
 		/// <inheritdoc />
-		public Task<VkCollection<User>> SearchAsync(UserSearchParams @params)
+		public Task<VkCollection<User>> SearchAsync(UserSearchParams @params, CancellationToken token = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>Search(@params: @params));
+			return TypeHelper.TryInvokeMethodAsync(() => Search(@params: @params), token);
 		}
 
 		/// <inheritdoc />
-		public Task<bool> IsAppUserAsync(long? userId)
+		public Task<bool> IsAppUserAsync(long? userId, CancellationToken token = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>IsAppUser(userId: userId));
+			return TypeHelper.TryInvokeMethodAsync(() => IsAppUser(userId: userId), token);
 		}
 
 		/// <inheritdoc />
-		public Task<ReadOnlyCollection<User>> GetAsync(IEnumerable<long> userIds
-															, ProfileFields fields = null
-															, NameCase nameCase = null)
+		public Task<ReadOnlyCollection<User>> GetAsync(IEnumerable<long> userIds, ProfileFields fields = null, NameCase nameCase = null,
+														CancellationToken token = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>
-					Get(userIds: userIds, fields: fields, nameCase: nameCase));
+			return TypeHelper.TryInvokeMethodAsync(() => Get(userIds, fields, nameCase), token);
 		}
 
 		/// <inheritdoc />
-		public Task<ReadOnlyCollection<User>> GetAsync(IEnumerable<string> screenNames
-															, ProfileFields fields = null
-															, NameCase nameCase = null)
+		public Task<ReadOnlyCollection<User>> GetAsync(IEnumerable<string> screenNames, ProfileFields fields = null,
+														NameCase nameCase = null, CancellationToken token = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>
-					Get(screenNames: screenNames, fields: fields, nameCase: nameCase));
+			return TypeHelper.TryInvokeMethodAsync(() => Get(screenNames, fields, nameCase), token);
 		}
 
 		/// <inheritdoc />
-		public Task<VkCollection<Group>> GetSubscriptionsAsync(long? userId = null
-																	, int? count = null
-																	, int? offset = null
-																	, GroupsFields fields = null)
+		public Task<VkCollection<Group>> GetSubscriptionsAsync(long? userId = null, int? count = null, int? offset = null,
+																GroupsFields fields = null, CancellationToken token = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>
-					GetSubscriptions(userId: userId
-							, count: count
-							, offset: offset
-							, fields: fields));
+			return TypeHelper.TryInvokeMethodAsync(() => GetSubscriptions(userId, count, offset, fields), token);
 		}
 
 		/// <inheritdoc />
-		public Task<VkCollection<User>> GetFollowersAsync(long? userId = null
-																, int? count = null
-																, int? offset = null
-																, ProfileFields fields = null
-																, NameCase nameCase = null)
+		public Task<VkCollection<User>> GetFollowersAsync(long? userId = null, int? count = null, int? offset = null,
+														ProfileFields fields = null, NameCase nameCase = null,
+														CancellationToken token = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>
-					GetFollowers(userId: userId
-							, count: count
-							, offset: offset
-							, fields: fields
-							, nameCase: nameCase));
+			return TypeHelper.TryInvokeMethodAsync(() => GetFollowers(userId, count, offset, fields, nameCase), token);
 		}
 
 		/// <inheritdoc />
-		public Task<bool> ReportAsync(long userId, ReportType type, string comment = "")
+		public Task<bool> ReportAsync(long userId, ReportType type, string comment = "", CancellationToken token = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>Report(userId: userId, type: type, comment: comment));
+			return TypeHelper.TryInvokeMethodAsync(() => Report(userId, type, comment), token);
 		}
 
 		/// <inheritdoc />
-		public Task<VkCollection<User>> GetNearbyAsync(UsersGetNearbyParams @params)
+		public Task<VkCollection<User>> GetNearbyAsync(UsersGetNearbyParams @params, CancellationToken token = default)
 		{
-			return TypeHelper.TryInvokeMethodAsync(func: () =>GetNearby(@params: @params));
+			return TypeHelper.TryInvokeMethodAsync(() => GetNearby(@params: @params), token);
 		}
 	}
 }
